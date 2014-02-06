09603586d9aaa0b4a96003249034077aedb1667a
Janne Hellsten <jjhellst@gmail.com>
2014-02-06 23:17:41 +0200

    Use later optparse-applicative and remove Control.Applicative import

--- db-util/Main.hs
+++ db-util/Main.hs
@@ -1,6 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-import           Control.Applicative
 import           Options.Applicative
 
 import           Types
@@ -12,17 +11,17 @@ import qualified Sqlite
 options :: Parser Opts
 options = Opts
        <$> strOption (long "output-db"
-                      & metavar "FILE"
-                      & help "Output database file or database name")
+                      <> metavar "FILE"
+                      <> help "Output database file or database name")
        <*> strOption (long "db"
-                      & metavar "DB"
-                      & help "Database implementation (sqlite, psql or mysql)")
+                      <> metavar "DB"
+                      <> help "Database implementation (sqlite, psql or mysql)")
 
 main = do
   options <- execParser $
                info (helper <*> options)
                (fullDesc
-                & progDesc "Generate test database")
+                <> progDesc "Generate test database")
   case dbImpl options of
     "sqlite" -> Sqlite.gen options
     "mysql"  -> Mysql.gen options
